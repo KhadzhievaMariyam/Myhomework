5621b1338a718f9454171df633f1b0508311997a
KhadzhievaMariyam <khadzhiieva2000@mail.ru>
2024-09-30 04:24:16 +0300

    Update module_1_3.py
    age

--- module_1_3.py
+++ module_1_3.py
@@ -1,10 +1,8 @@
 name = "Mariyam"
 print ("Name:" ,name)
-age = "24"
+age = 24
 print ("Age:" ,age)
-b=24
-c=1
-newage = b+c
-print ("New age:" , newage)
+age = age + 1
+print ("New age:" , age)
 isstudent = "True"
 print ("Is  student:", isstudent)
